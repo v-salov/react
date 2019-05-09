29da6a151f03ccb18429a409fc7b09743854622d
v-salov <45938320+v-salov@users.noreply.github.com>
2019-05-09 17:01:28 +0300

    Update error-indicator.js

--- phone-store/src/components/error-indicator/error-indicator.js
+++ phone-store/src/components/error-indicator/error-indicator.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import './error-indicator.css';
+//import './error-indicator.css';
 
 const ErrorIndicator = () => {
   return <h2 className="text-center text-danger mt-2">Error!</h2>;
